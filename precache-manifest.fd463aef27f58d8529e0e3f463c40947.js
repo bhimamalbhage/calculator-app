ac66d088764828c4ed99c02a8d353df897f134d5
Bhimashankar <bhimamalbhage@hotmail.com>
2020-06-08 19:55:35 +0530

    Updates

---
+++ precache-manifest.fd463aef27f58d8529e0e3f463c40947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce4e582fe474378f85418e2a3e174fbd",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "2e00c703bb7442d40b8b",
+    "url": "/calculator-app/static/css/main.c20290bf.chunk.css"
+  },
+  {
+    "revision": "7e95990b9b6012bf1ac6",
+    "url": "/calculator-app/static/js/2.3aa5db08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app/static/js/2.3aa5db08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e00c703bb7442d40b8b",
+    "url": "/calculator-app/static/js/main.3b7cc14b.chunk.js"
+  },
+  {
+    "revision": "bff8ed9160da4f5786cd",
+    "url": "/calculator-app/static/js/runtime-main.d9187316.js"
+  }
+]);
